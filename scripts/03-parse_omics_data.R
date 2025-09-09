ceb4b7226f53f0d796700c87aaffed95e2523e94
ftucos <fran.tucci@gmail.com>
2025-09-09 12:26:27 +0200

    feat: explore multiple survival cutoff

--- scripts/03-parse_omics_data.R
+++ scripts/03-parse_omics_data.R
@@ -6,16 +6,19 @@ library(ggstatsplot)
 library(ggbeeswarm)
 library(ggpubr)
 library(ggh4x)
+library(survival)
+library(survminer)
+library(broom)
 
 # Load Load Data
-exp <- fread("data/processed/deduplicated_and_filtered-data_mrna_illumina_microarray.tsv.gz") %>%
+exp <- fread("data/processed/deduplicated_and_filtered-data_mrna_illumina_microarray.tsv.gz")
   
 metadata <- readRDS("data/processed/METABRIC-metadata.rds")
 
 scores <- fread("data/interim/GSVA/singscore_scores.tsv")
 
 # list genes of interest to be used for plots
-exp_of_int <- c("CDK12")
+exp_of_int <- c("ABCC1", "SRCIN1")
 
 gene_expression <- exp %>%
   filter(external_gene_name %in% exp_of_int) %>%
@@ -25,9 +28,9 @@ gene_expression <- exp %>%
   rename_all(~paste0(., "_EXP")) %>%
   rownames_to_column("PATIENT_ID")
 
-CNA_of_int <- c("CDK12")
+CNA_of_int <- c("SRCIN1")
 
-gene_CNA <- fread("data/brca_metabric/data_cna.txt") %>%
+gene_CNA <- fread("data/raw_data/brca_metabric/data_cna.txt") %>%
   select(-Entrez_Gene_Id) %>%
   filter(Hugo_Symbol %in% CNA_of_int) %>%
   column_to_rownames("Hugo_Symbol") %>%
@@ -57,7 +60,7 @@ metadata.scores_long <-  metadata %>%
 ggplot(metadata.scores_long, aes(x=ABCC1_EXP, y=GSVA, group=Signature)) +
   geom_point(size = 0.3) +
   stat_smooth(method="lm", linewidth =0.7) +
-  stat_cor(label.x = 6.5, label.y=-0.6, size = 8/.pt)+
+  stat_cor(label.x = 6.5, label.y=0.1, size = 8/.pt)+
   theme_bw(8) +
   facet_wrap(~Signature) +
   force_panelsizes(rows = unit(4.5, "cm"), cols = unit(5.5, "cm"))
@@ -67,13 +70,12 @@ ggsave("results/METABRIC/METABRIC-ABCC1_expression_and_stemness_signatures.pdf",
 ggplot(metadata.scores_long %>% filter(Pam50 != "NC"), aes(x=ABCC1_EXP, y=GSVA, group=Signature)) +
   geom_point(size = 0.3) +
   stat_smooth(method="lm", linewidth =0.7) +
-  stat_cor(label.x = 6.5, label.y=-0.6, size = 8/.pt)+
+  stat_cor(label.x = 6.5, label.y=0.1, size = 8/.pt)+
   theme_bw(8) +
   facet_grid(Pam50~Signature) +
   force_panelsizes(rows = unit(4.5, "cm"), cols = unit(5.5, "cm"))
 
 ggsave("results/METABRIC/METABRIC-ABCC1_expression_and_stemness_signatures-by_subtype.pdf", width = 60, height = 30, unit = "cm" )
-
 
 # ABCC1 expression and SRCIN1 CNA ---------
 ggstatsplot::ggbetweenstats(metadata.1 %>% filter(Pam50 != "Her2"),
@@ -85,3 +87,48 @@ metadata.1 %>% filter(Pam50 != "NC") %>%
   mutate(Pam50 = factor(Pam50, levels = c("LumA", "LumB", "Her2", "Basal", "Normal"))) %>%
   lm(ABCC1_EXP ~ SRCIN1_CNA + Pam50, data = .) %>%
     summary()
+
+
+# explore non-linear relationship with survival --------------------------------
+cox_res <- coxph(Surv(OS_years, OS) ~ pspline(SRCIN1_EXP), data = metadata.1)
+cox_res %>% termplot()
+
+# explore multiple survival cutoff and thresholds ------------------------------
+
+source("R/extract_cox_stats.R")
+endpoint <- c("OS", "DSS", "RFS")
+variable <- c("SRCIN1_EXP", "ABCC1_EXP")
+quantiles <- list(0.1, 0.15, 0.25, 0.33, 0.5, 0.66, 0.75, 0.85, 0.9, "best")
+
+# grid of model parameters to test
+test_grid <- expand_grid(endpoint = endpoint,
+                         variable = variable,
+                         selected_quantile = quantiles)
+
+# specify x as data for creating a partial function
+map_funciton <- partial(extract_cox_stats, data = metadata.1)
+res <- pmap_dfr(test_grid, map_funciton) %>%
+  ungroup() %>%
+  # highlight best quantile for each variable/endpoint combination
+  mutate(best = ifelse(p.value == min(p.value), TRUE, FALSE), .by = c(variable, endpoint))
+
+# plot thresholds results
+res %>%
+  # fill missing values for eventual vest quantiles to plot them as missing
+  complete(variable, endpoint, quantile) %>%
+  # add pvalue label
+  mutate(signif = case_when(
+    p.value < 0.001 ~ "***",
+    p.value < 0.01 ~ "**",
+    p.value < 0.05 ~ "*",
+    TRUE ~ "")) %>%
+  ggplot(aes(y=as.character(quantile), x=endpoint, fill = HR)) +
+  geom_tile() +
+  geom_text(aes(label = signif)) +
+  facet_wrap(~variable) +
+  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0, na.value = "grey90") +
+  scale_x_discrete(expand = c(0,0)) +
+  scale_y_discrete(expand = c(0,0)) +
+  ylab("Quantile") +
+  theme_bw() +
+  theme(panel.grid = element_blank())
