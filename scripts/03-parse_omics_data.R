40cdcf87a4765aef9b5a2ea5e43592682fd93f6c
ftucos <fran.tucci@gmail.com>
2025-09-25 15:54:43 +0200

    chore: replace example genes with canonical genes in BRCA

--- scripts/03-parse_omics_data.R
+++ scripts/03-parse_omics_data.R
@@ -18,7 +18,7 @@ metadata <- readRDS("data/processed/METABRIC-metadata.rds")
 scores <- fread("data/interim/GSVA/singscore_scores.tsv")
 
 # list genes of interest to be used for plots
-exp_of_int <- c("BAG3", "HSPB8")
+exp_of_int <- c("MKI67", "ESR1", "PGR", "ERBB2")
 
 gene_expression <- exp %>%
   filter(external_gene_name %in% exp_of_int) %>%
@@ -28,7 +28,7 @@ gene_expression <- exp %>%
   rename_all(~paste0(., "_EXP")) %>%
   rownames_to_column("PATIENT_ID")
 
-CNA_of_int <- c("SRCIN1")
+CNA_of_int <- c("ERBB2")
 
 gene_CNA <- fread("data/raw_data/brca_metabric/data_cna.txt") %>%
   select(-Entrez_Gene_Id) %>%
@@ -57,16 +57,16 @@ metadata.scores_long <-  metadata %>%
   full_join(scores %>% gather(key="Signature", value = "GSVA", -1))
 
 # plot --------------
-ggplot(metadata.1, aes(x=SCMOD2, y=BAG3_EXP, color=SCMOD2)) +
+ggplot(metadata.1, aes(x=SCMOD2, y=MKI67_EXP, color=SCMOD2)) +
   geom_point(size = 0.3) +
   stat_smooth(method="lm", linewidth =0.7) +
   stat_cor(label.x = 6.5, label.y=0.1, size = 8/.pt)+
   theme_bw(8) +
   force_panelsizes(rows = unit(4.5, "cm"), cols = unit(5.5, "cm"))
 
-ggsave("results/METABRIC/METABRIC-ABCC1_expression_and_stemness_signatures.pdf", width = 20, height = 20, unit = "cm" )
+ggsave("results/METABRIC/METABRIC-gene_expression_and_stemness_signatures.pdf", width = 20, height = 20, unit = "cm" )
 
-ggplot(metadata.scores_long %>% filter(Pam50 != "NC"), aes(x=ABCC1_EXP, y=GSVA, group=Signature)) +
+ggplot(metadata.scores_long %>% filter(Pam50 != "NC"), aes(x=MKI67_EXP, y=GSVA, group=Signature)) +
   geom_point(size = 0.3) +
   stat_smooth(method="lm", linewidth =0.7) +
   stat_cor(label.x = 6.5, label.y=0.1, size = 8/.pt)+
@@ -74,29 +74,29 @@ ggplot(metadata.scores_long %>% filter(Pam50 != "NC"), aes(x=ABCC1_EXP, y=GSVA, 
   facet_grid(Pam50~Signature) +
   force_panelsizes(rows = unit(4.5, "cm"), cols = unit(5.5, "cm"))
 
-ggsave("results/METABRIC/METABRIC-ABCC1_expression_and_stemness_signatures-by_subtype.pdf", width = 60, height = 30, unit = "cm" )
+ggsave("results/METABRIC/METABRIC-gene_expression_and_stemness_signatures-by_subtype.pdf", width = 60, height = 30, unit = "cm" )
 
 # ABCC1 expression and SRCIN1 CNA ---------
 ggstatsplot::ggbetweenstats(metadata.1 %>% filter(Pam50 != "Her2"),
-                            x=SRCIN1_CNA, y=ABCC1_EXP,
+                            x=PGR_EXP, y=MKI67_EXP,
                             type = "parametric", bf.message = F, centrality.plotting = F)
 
 metadata.1 %>% filter(Pam50 != "NC") %>%
-  mutate(SRCIN1_CNA = factor(SRCIN1_CNA, levels = c("Diploid", "Deep/Homozygous Deletion", "Shallow Deletion", "Gain", "Amplification"))) %>%
+  mutate(ERBB2_CNA = factor(ERBB2_CNA, levels = c("Diploid", "Deep/Homozygous Deletion", "Shallow Deletion", "Gain", "Amplification"))) %>%
   mutate(Pam50 = factor(Pam50, levels = c("LumA", "LumB", "Her2", "Basal", "Normal"))) %>%
-  lm(ABCC1_EXP ~ SRCIN1_CNA + Pam50, data = .) %>%
+  lm(ERBB2_EXP ~ ERBB2_CNA + Pam50, data = .) %>%
     summary()
 
 
 # explore non-linear relationship with survival --------------------------------
-cox_res <- coxph(Surv(OS_years, OS) ~ pspline(SRCIN1_EXP), data = metadata.1)
-cox_res %>% termplot()
+cox_res <- coxph(Surv(OS_years, OS) ~ pspline(PGR_EXP), data = metadata.1)
+cox_res %>% termplot(rug = TRUE, se = TRUE)
 
 # explore multiple survival cutoff and thresholds ------------------------------
 
 source("R/extract_cox_stats.R")
 endpoint <- c("OS", "DSS", "RFS")
-variable <- c("SRCIN1_EXP", "ABCC1_EXP")
+variable <- c("MKI67_EXP", "PGR_EXP")
 quantiles <- list(0.1, 0.15, 0.25, 0.33, 0.5, 0.66, 0.75, 0.85, 0.9, "best")
 
 # grid of model parameters to test
